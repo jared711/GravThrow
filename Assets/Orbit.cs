7713d63edc8d3c4319aafadf4aba2603dd311d8b
Jared Blanchard <jared711@gmail.com>
2022-04-11 10:32:21 -0700

    Main menu done, starting on orbital elements

---
+++ Assets/Orbit.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+/*    // Start is called before the first frame update
+    public float a;
+    public float p;
+    public float e;
+    public float i;
+    public float Omega;
+    public float omega;
+    public float nu;
+  */  
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
